f35290815a45282538634d02db588b2736916383
1argeD <gkswhdgur1234@naver.com>
2022-12-23 20:04:27 +0900

    <feat>redis 공부자료 일단 세이프

---
+++ src/test/java/Example.java
@@ -0,0 +1,25 @@
+import org.example.Entity.Entity;
+import org.example.Repository.Repository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Example {
+
+    @Autowired
+    private Repository repo;
+
+    @Test
+    void test(){
+        Entity gel = new Entity(500,"thailand");
+
+        repo.save(gel);
+
+        repo.findById(gel.getId());
+        /*Entity에 @RedisHash 에 정의되어 있는 keyspace (Physiogel)에 속한 키의 갯수를 구함*/
+        repo.count();
+
+        repo.delete(gel);
+    }
+}
