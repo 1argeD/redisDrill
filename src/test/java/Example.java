586666c12d4368da7db2ba4ce3c3ba8e4f4e1d73
1argeD <gkswhdgur1234@naver.com>
2022-12-27 20:27:59 +0900

    <feat>redis 레디스와 자바 버전 맞춤

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -1,16 +1,17 @@
 import org.example.Entity.Entity;
 import org.example.Repository.Repository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Example {
-
+    @Autowired
     private Repository repo;
 
     @Test
     void test(){
         Entity gel = new Entity(500,"thailand");
 
-        repo.save()
+        repo.save(gel);
 
         repo.findById(gel.getId());
         /*Entity에 @RedisHash 에 정의되어 있는 keyspace (Physiogel)에 속한 키의 갯수를 구함*/
